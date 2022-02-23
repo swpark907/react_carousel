27699ee67ef44f54a94592f6905c40f170b465c0
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-02-23 20:28:33 +0900

    update slide function when btn clicked

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
-import './App.css'
-import images from './images';
+import React from "react";
+import { useState, useEffect, useRef } from "react";
+import "./App.css";
+import images from "./images";
 
 const App = () => {
+  let totalSlides = 4;
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  function prevSlide() {
+    currentSlide <= 0
+      ? setCurrentSlide(totalSlides)
+      : setCurrentSlide(currentSlide - 1);
+  }
+
+  function nextSlide() {
+    currentSlide >= totalSlides
+      ? setCurrentSlide(0)
+      : setCurrentSlide(currentSlide + 1);
+  }
+
   return (
-    <div className='App'>
+    <div className="App">
       <div className="container">
-      <div className="carousel">
-        {images.map((image) => 
-          <div className="item">
-            <img className="image" src={image} alt={image} />
-          </div>
-        )}
-      </div>
-      <div className="btn-container">
-        <button className="prev">prev</button>
-        <button className="next">next</button>
-      </div>
+        <div className="carousel" style={{transform:`translateX(-${currentSlide}00%)`}}>
+          {images.map((image, index) => (
+            <div className="item" key={index} >
+              <img className="image" src={image} alt={image} />
+            </div>
+          ))}
+        </div>
+        <div className="btn-container">
+          <button className="prev" onClick={prevSlide}>
+            prev
+          </button>
+          <button className="next" onClick={nextSlide}>
+            next
+          </button>
+        </div>
       </div>
     </div>
   );
