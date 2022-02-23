483d9bcbc7d5cb30bd851362264a79e8c907412c
swpark907 <76856321+swpark907@users.noreply.github.com>
2022-02-23 16:55:25 +0900

    update element of App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 import './App.css'
+import images from './images';
 
 const App = () => {
   return (
     <div className='App'>
-      
+      <div className="container">
+      <div className="carousel">
+        {images.map((image) => 
+          <div className="item">
+            <img className="image" src={image} alt={image} />
+          </div>
+        )}
+      </div>
+      <div className="btn-container">
+        <button className="prev">prev</button>
+        <button className="next">next</button>
+      </div>
+      </div>
     </div>
   );
 };
